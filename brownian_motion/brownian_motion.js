67e5fbaa8f6da059889f6257f7f4717360f4187a
DanielTemesgen <40549299+DanielTemesgen@users.noreply.github.com>
2018-08-23 00:03:18 +0100

    Intersection added.

--- brownian_motion/brownian_motion.js
+++ brownian_motion/brownian_motion.js
@@ -1,64 +1,69 @@
 let bubbles = []; // instantiating the array where the bubbles will be.
 let bubble_properties = {
-    def_colour : 0,
-    new_colour : 255
+    def_colour: 0,
+    new_colour: 255
 }
 
 function setup() { // this sets up the bubbles and assigns their initial properties.
-  createCanvas(displayWidth, displayHeight);
-  for (let i = 0; i < 2000; i++) {
-      bubbles[i] = new Bubble();
-  }
+    createCanvas(displayWidth, displayHeight);
+    for (let i = 0; i < 200; i++) {
+        bubbles[i] = new Bubble();
+    }
 }
 
 function draw() { // this sets the background and the move and show methods.
-  background(100);
-  for (let b of bubbles) {
-      if (b.rollover(mouseX, mouseY)){
-          b.change_colour(255)
-      } else {
-          b.change_colour()
-      }
-//      if (b.intersect(bubble)){
-//          background()
-//      }
-      b.move();
-      b.show();
-  }
+    background(100);
+    for (let b of bubbles) {
+        b.move();
+        b.show();
+        overlap = false;
+        for (let other of bubbles) {
+            if (b !== other && b.intersects(other)) {
+                overlap = true;
+            }
+        }
+        if (overlap) {
+            b.change_colour(50)
+        }
+        if (b.rollover(mouseX, mouseY)) {
+            b.change_colour(255)
+        } else {
+            b.change_colour()
+        }
+    }
 }
 
-function mouseDragged(){
-    let r = random(bubble_properties.min_size, bubble_properties.max_size);
-    let b = new Bubble(mouseX, mouseY, r);
+function mouseDragged() {
+    let b = new Bubble(mouseX, mouseY);
     bubbles.push(b)
 }
 
 class Bubble { //this is a generic template used to define objects
-  constructor(x = random(width), y = random(height), r=random(10,30), def_colour = 0){  // data goes in here
-      this.x = x;
-      this.y = y;
-      this.r = r;
-      this.colour = def_colour;
-      this.transparency = 10;
-  }
-  change_colour(colour = bubble_properties.def_colour){
-      this.colour = colour
-  }
-rollover(px, py){
-    let d = dist(px, py, this.x, this.y);
-    return d < this.r
-}
-intersect(other){
-    let d = (this.x, this.y, other.x, other.y)
-    return (d < this.x + other.x)
-}
-move(){
-    this.x += random(-5, 5);
-    this.y += random(-5, 5);
-}
-show(){
-    noStroke();
-    fill(0, 0, this.colour, this.transparency)
-    ellipse(this.x, this.y, this.r*2)
-  }
+    constructor(x = random(width), y = random(height), r = random(10, 30), def_colour = 0) { // data goes in here
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.colour = 0;
+        this.transparency = 100;
+    }
+    change_colour(colour = bubble_properties.def_colour) {
+        this.colour = colour
+    }
+    rollover(px, py) {
+        let d = dist(px, py, this.x, this.y);
+        return d < this.r
+    }
+    intersects(o) {
+        let d = (this.x, this.y, o.x, o.y);
+        return (d < this.r + o.r)
+    }
+    move() {
+        this.x += random(-3, 3);
+        this.y += random(-3, 3);
+    }
+    show() {
+        noStroke();
+        fill(0, 0, this.colour, this.transparency)
+        ellipse(this.x, this.y, this.r * 2)
+    }
 }
